d2775ccfba588f73cfa58db8e1c6e8061e370dd6
kennedy_blackPC2 <kennedy.lopes@ufersa.edu.br>
2026-09-11 08:41:15 -0300

    main atualizado com a inserção automática

--- bst.c
+++ bst.c
@@ -10,46 +10,33 @@ typedef struct No {
 No *inserir(No *raiz, int valor);
 
 int main() {
-  No *nos[6];
-  nos[0] = malloc(sizeof(struct No));
-  nos[0]->esq = 0;
-  nos[0]->dir = 0;
-  nos[0]->valor = 10;
+  // 10 - 30 - 40 - 8 - 25 - 14
+  No *raiz = 0;
+  raiz = inserir(raiz, 10);
+  raiz = inserir(raiz, 30);
+  raiz = inserir(raiz, 40);
+  raiz = inserir(raiz, 8);
+  raiz = inserir(raiz, 14);
 
-  nos[0]->dir = malloc(sizeof(struct No));
-  nos[1] = nos[0]->dir;
-  nos[1]->dir = 0;
-  nos[1]->esq = 0;
-  nos[1]->valor = 30;
 
-  nos[1]->dir = malloc(sizeof(struct No));
-  nos[2] = nos[1]->dir;
-  nos[2]->dir = 0;
-  nos[2]->esq = 0;
-  nos[2]->valor = 40;
 }
-//Atenção: Não estou utilizando o cast para o mallocs
+// Atenção: Não estou utilizando o cast para o mallocs
 No *inserir(No *raiz, int valor) {
   if (raiz == 0) {
     No *novo = malloc(sizeof(No));
-    if(novo == 0){
-        printf("Erro: Não foi possível crirar No");
-        exit(1);
+    if (novo == 0) {
+      printf("Erro: Não foi possível crirar No");
+      exit(1);
     }
     novo->dir = 0;
     novo->esq = 0;
     novo->valor = valor;
     return novo;
   }
-  if(valor < raiz->valor){
+  if (valor < raiz->valor) {
     raiz->esq = inserir(raiz->esq, valor);
-  }else if(valor > raiz->valor){
+  } else if (valor > raiz->valor) {
     raiz->dir = inserir(raiz->dir, valor);
   }
   return raiz;
 }
-
-/*
-Inserir manualmente:
-10 - 30 - 40 - 8 - 25 - 14
-*/
