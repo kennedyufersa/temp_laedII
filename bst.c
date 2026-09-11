702269ce7dc8b4075636638d890c640383144de8
kennedy_blackPC2 <kennedy.lopes@ufersa.edu.br>
2026-09-11 08:46:43 -0300

    Organizando a função inserir

--- bst.c
+++ bst.c
@@ -8,30 +8,20 @@ typedef struct No {
 } No;
 
 No *inserir(No *raiz, int valor);
+No *criarNo(int);
 
 int main() {
-  // 10 - 30 - 40 - 8 - 25 - 14
   No *raiz = 0;
   raiz = inserir(raiz, 10);
   raiz = inserir(raiz, 30);
   raiz = inserir(raiz, 40);
   raiz = inserir(raiz, 8);
   raiz = inserir(raiz, 14);
-
-
 }
 // Atenção: Não estou utilizando o cast para o mallocs
 No *inserir(No *raiz, int valor) {
   if (raiz == 0) {
-    No *novo = malloc(sizeof(No));
-    if (novo == 0) {
-      printf("Erro: Não foi possível crirar No");
-      exit(1);
-    }
-    novo->dir = 0;
-    novo->esq = 0;
-    novo->valor = valor;
-    return novo;
+    return criarNo(valor);
   }
   if (valor < raiz->valor) {
     raiz->esq = inserir(raiz->esq, valor);
@@ -39,4 +29,16 @@ No *inserir(No *raiz, int valor) {
     raiz->dir = inserir(raiz->dir, valor);
   }
   return raiz;
+}
+
+No *criarNo(int valor) {
+  No *novo = malloc(sizeof(No));
+  if (novo == 0) {
+    printf("Erro: Não foi possível crirar No");
+    exit(1);
+  }
+  novo->dir = 0;
+  novo->esq = 0;
+  novo->valor = valor;
+  return novo;
 }
