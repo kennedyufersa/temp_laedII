ebab01dd75c4c21dec40b08c70581382af4d213d
kennedy_blackPC2 <kennedy.lopes@ufersa.edu.br>
2026-09-11 08:14:42 -0300

    inserção manual de alguns valores

--- bst.c
+++ bst.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct No {
   int valor;
@@ -8,5 +9,18 @@ typedef struct No {
 
 
 int main(){
-    
+    No *novo = malloc(sizeof(struct No));
+    novo->esq = 0;
+    novo->dir = 0;
+    novo->valor = 10;
+
+    novo->dir = malloc(sizeof(struct No));
+    novo->dir->dir = 0;
+    novo->dir->esq = 0;
+    novo->dir->valor = 30;
 }
+
+/*
+Inserir manualmente:
+10 - 30 - 40 - 8 - 25 - 14
+*/
